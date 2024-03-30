b38f168e7b60949e4c435a86796d19c6fd4d4d9c
mrbillgates2001 <shahboznabiyev2001@gmail.com>
2024-03-31 04:14:53 +0500

    design added

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,9 +4,10 @@ import { Link, Navigate } from "react-router-dom";
 const Profile = () => {
 	return (
 		<div className="flex flex-col justify-center items-center mx-auto bg-slate-300 w-50 mt-16 py-10 rounded-full bg-opacity-70">
-			<div className="flex items-center gap-1"></div>
-			<img src="/avatar.png" alt="" width={100} />
-			<h1>Profile</h1>
+			<div className="flex flex-col items-center gap-1">
+				<img src="/avatar.png" alt="" width={100} />
+				<h1>Profile</h1>
+			</div>
 
 			<div className="flex items-center gap-3">
 				<h3>Username: </h3>
